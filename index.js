9ddb951f36dd83d2e4d67895f4db0c348d5a2586
Samuel Anderson <sams1a2m34@gmail.com>
2022-12-18 13:33:24 -0500

    list databases method implementation

--- index.js
+++ index.js
@@ -75,4 +75,11 @@ async function dbmanin() {
   }
 }
 
+async function listDatabases(client){
+  databasesList = await client.db().admin().listDatabases();
+
+  console.log("Databases:");
+  databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+
 dbmanin().catch(console.error);
