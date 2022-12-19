5436554ecc3a65b72cf9a4a3dc609898dd2b6ad2
Samuel Anderson <sams1a2m34@gmail.com>
2022-12-18 23:26:22 -0500

    mongo update test

--- collections/abilityScores.js
+++ collections/abilityScores.js
@@ -1,15 +1,28 @@
-var request = require('sync-request');
+var request = require ('request');
 const dbconnect = require('../database/connection');
 
 var db = dbconnect.getDb();
 
 module.exports = {
     Request: function () {
-        var res = request('POST', 'https://raw.githubusercontent.com/5e-bits/5e-database/main/src/5e-SRD-Ability-Scores.json', { json: true, });
-        var data = JSON.parse(res.getBody('utf8'));
+        request ({
+            url: 'https://raw.githubusercontent.com/5e-bits/5e-database/main/src/5e-SRD-Ability-Scores.json',
+            json: true
+        }, (error, response, body) => {
+            if(error || response.statusCode != 200){
+                console.log(error)
+            } else {
+                if (body.length > 0) {
+                    // Create index before insert
+                    try {
+                        db.collection('abilityScores').createIndex({"index":1},{ unique: true } );
+                    } catch (e) { } // Index already exists
 
-        for (var node in data) {
-            db.collection('abilityScores').insert(data[node], { upsert: true });
-        }
+                    for (var node in body) {
+                        db.collection('abilityScores').updateOne({ "index": body[node] }, body[node], { upsert: true })
+                    } 
+                }
+            }
+        })
     }
 }
